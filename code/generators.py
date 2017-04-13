36d366f90564068aa978082e315cf16853f86eba
Kyle Kloster <kkloste@users.noreply.github.com>
2017-04-13 09:02:50 -0400

    Add license info to .py files (#8)
    * license info in init
    
    * license info in polygraph.py
    
    * license info in graphs.py
    
    * license info for linalg.py
    
    * license info for generators.py
    
    * Added license info to walk_class_table
    
    * Adding license info to header of all remaining .py files
    
    * license info in init
    
    * license info in polygraph.py
    
    * license info in graphs.py
    
    * license info for linalg.py
    
    * license info for generators.py

--- code/generators.py
+++ code/generators.py
@@ -1,3 +1,8 @@
+#
+# This file is part of spiderdonuts, https://github.com/TheoryInPractice/spiderdonuts/,
+# and is Copyright (C) North Carolina State University, 2017. It is licensed
+# under the three-clause BSD license; see LICENSE.
+#
 """A collection of functions used to generate pre-existing graphs."""
 
 # Imports
